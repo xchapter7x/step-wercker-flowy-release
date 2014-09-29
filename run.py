6dbf7275050aa5859c1d1f1d765dddb0a7810f93
John Calabrese <jcalabrese@pivotal.io>
2014-09-29 16:07:08 -0400

    [#79692868] working on better coverage
    once the coverage is better, we can start testing this out

--- run.py
+++ run.py
@@ -1,21 +1,94 @@
 import os
+import subprocess
+import sys
 
 required_fields = [
   "WERCKER_FLOWY_DEPLOY_ACTION",
   "WERCKER_FLOWY_DEPLOY_TAG_VARIABLE_NAME"
 ]
 
-field_flag_actions = {
-  "WERCKER_FLOWY_DEPLOY_ACTION":              lambda v: v if v != None else "",
-  "WERCKER_FLOWY_DEPLOY_TAG_VARIABLE_NAME":   lambda v: v if v != None else "",
-  "WERCKER_FLOWY_DEPLOY_ACTIVE":              lambda v: v if v != None else True,
-  "WERCKER_FLOWY_DEPLOY_TAG_REGEX":           lambda v: v if v != None else "v([0-9]{1,2})\.([0-9]{1,2})\.([0-9]{1,4})",
-  "WERCKER_FLOWY_DEPLOY_START_VERSION":       lambda v: v if v != None else "v01.00.0001",
-  "WERCKER_FLOWY_DEPLOY_TAG_MESSAGE":         lambda v: v if v != None else "Auto-Tag"
+field_flags = {
+  "WERCKER_FLOWY_DEPLOY_ACTION":              os.environ.get("WERCKER_FLOWY_DEPLOY_ACTION", ""),
+  "WERCKER_FLOWY_DEPLOY_TAG_VARIABLE_NAME":   os.environ.get("WERCKER_FLOWY_DEPLOY_TAG_VARIABLE_NAME", ""),
+  "WERCKER_FLOWY_DEPLOY_ACTIVE":              os.environ.get("WERCKER_FLOWY_DEPLOY_ACTIVE", True),
+  "WERCKER_FLOWY_DEPLOY_TAG_REGEX":           os.environ.get("WERCKER_FLOWY_DEPLOY_TAG_REGEX", "v([0-9]{1,2})\.([0-9]{1,2})\.([0-9]{1,4})"),
+  "WERCKER_FLOWY_DEPLOY_START_VERSION":       os.environ.get("WERCKER_FLOWY_DEPLOY_START_VERSION", "v01.00.0001"),
+  "WERCKER_FLOWY_DEPLOY_TAG_MESSAGE":         os.environ.get("WERCKER_FLOWY_DEPLOY_TAG_MESSAGE", "Auto-Generated-Tag")
 }
 
+def required_field_check(required_fields, env_variables):
+  checkPassed = True
+
+  for fieldname in required_fields:
+
+    if not env_variables.has_key(fieldname):
+      checkPassed = False
+      print("Required ENV Variable not set: %s" % (fieldname))
+    
+  return checkPassed
+
+def version_increment_string(current_version):
+  return """echo """+current_version+""" | awk -F. -v OFS=. 'NF==1{print ++$NF};NF>1{if(length($NF+1)>length($NF))$(NF-1)++;$NF=sprintf("%0*d",length($NF),($NF+1)%(10^length($NF))); print}'"""
+
+def tag_match_string():
+  output = ""
+  return ("git tag -l | egrep \"%s\"" % field_flags["WERCKER_FLOWY_DEPLOY_TAG_REGEX"])
+
+def make_array_from_stdout(output):
+  output = output.rstrip("\n").split("\n")
+  return output
+
+def system_call(cmdString):
+  output = []
+  err = False
+
+  try:
+    stdout = subprocess.check_output(cmdString, shell=True)
+    output = make_array_from_stdout(stdout)
+    
+  except subprocess.CalledProcessError as e:
+    output = "error: {0}".format(e)
+    err = True
+
+  return (output, err)
+
+def get_current_tag(functor):
+  current_tag = None
+  out, err = functor(tag_match_string())
+
+  if not err:
+    tags = out.sort(reverse=True)
+    current_tag = tags[0]
+
+  return current_tag
+
+def get_next_tag(functor):
+  current = get_current_tag(functor)
+  
+  if current == None:
+    current = field_flags["WERCKER_FLOWY_DEPLOY_START_VERSION"]
+
+  out, err = functor(version_increment_string(current))
+  return out[0]
+
+
 def run():
-  print("do nothing")
+  exitcode = 0
+
+  if not required_field_check(required_fields, os.environ):
+    print("do nothing")
+    out, err = system_call(tag_match_string())
+    print(out, err)
+    out, err = system_call(version_increment_string("v1.0.4"))
+    print(out, err)
+    print( get_current_tag(system_call) )
+    print(  get_next_tag(system_call) )
+
+  else:
+    print("FAILED!!!")
+    exitcode = 1
+
+  exit(exitcode)
 
 if __name__ == '__main__':
   run()
--- run_unittest.py
+++ run_unittest.py
@@ -1,12 +1,117 @@
 import unittest
 import run
+import os
+
+success_output = "some success output"
+failure_output = "some error output"
+
+def system_call_mock_success(cmdString):
+  return (success_output, False)
+
+def system_call_mock_failure(cmdString):
+  return (failure_output, True)
+
+def current_tag_functor_default_mock(cmdString):
+  return ([cmdString], True)
 
 class ParserTestCase(unittest.TestCase):
-    """Tests for `run.py`."""
+  """Tests for `run.py`."""
+  
+  @classmethod
+  def setUpClass(self):
+    self.required_fields = [
+      "WERCKER_FLOWY_DEPLOY_ACTION",
+      "WERCKER_FLOWY_DEPLOY_TAG_VARIABLE_NAME"
+    ]
 
-    def test_weAreTesting(self):
-        """run something"""
-        self.assertTrue(True)
+  def test_system_call_success(self):
+    """
+    success should yield False error and
+    an array matching out control
+    """
+    control = ["hi there"]
+    o, e = run.system_call("echo "+control[0])
+    self.assertFalse(e)
+    self.assertEqual(o, control, "{0} != {1}".format(o, control) )
+
+  def test_system_call_failure(self):
+    """
+    failure result from the command sent to
+    system_call should yield a 'True' error value 
+    response
+    """
+    o, e = run.system_call("exit 1")
+    self.assertTrue(e)
+
+  def test_make_array_from_stdout(self):
+    """
+    make_array_from_stdout should split the
+    string output properly
+    and clean trailing empty values
+    """
+    controlString = "hi\nthere\nmy test\nis done\n"
+    controlResult = controlString.split("\n")
+    del controlResult[-1]
+    out = run.make_array_from_stdout(controlString)
+    self.assertEqual(out, controlResult, "{0} != {1}".format(out, controlResult) )
+
+  def test_make_array_from_stdout_noempty(self):
+    """
+    if no trailing empties then no
+    deleting of record  
+    """
+    controlString = "hi\nthere\nmy test\nis done"
+    controlResult = controlString.split("\n")
+    out = run.make_array_from_stdout(controlString)
+    self.assertEqual(out, controlResult, "{0} != {1}".format(out, controlResult) )
+
+  def test_required_field_check(self):
+    """
+    should return False if all required fields
+    are not set
+    """
+    mockEnv = {}
+    response = run.required_field_check(self.required_fields, mockEnv)
+    self.assertFalse(response)
+
+  def test_required_field_check_partial(self):
+    """
+    should return False if some fields
+    are not set
+    """
+    mockEnv = {}
+    mockEnv["WERCKER_FLOWY_DEPLOY_ACTION"] = "test"
+    response = run.required_field_check(self.required_fields, mockEnv)
+    self.assertFalse(response)
+
+  def test_required_field_check_clean(self):
+    """
+    should return true if all requireds are set
+    """
+    mockEnv = {}
+    mockEnv["WERCKER_FLOWY_DEPLOY_ACTION"] = "test"
+    mockEnv["WERCKER_FLOWY_DEPLOY_TAG_VARIABLE_NAME"] = "test"
+    response = run.required_field_check(self.required_fields, mockEnv)
+    self.assertTrue(response)
+
+  #def test_get_current_tag(self):
+    #"""
+    #this should return the current tag from 
+    #the given functor when the functor is
+    #passed the proper string
+    #"""
+    #response = run.get_current_tag(current_tag_functor_default_mock)
+    #print(response)
+    #self.assertTrue(response)
+
+"""
+    def test_tag_match_string:
+    def test_version_increment_string:
+    
+    
+    def test_get_next_tag
+    def test_run
+"""
 
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
