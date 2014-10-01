82fdade96a9897c46e4f842aedde1dc94410c6a3
John Calabrese <jcalabrese@pivotal.io>
2014-10-01 09:50:14 -0400

    [#79692868] need to fetch
    adding fetch back in to the finish
    call, otherwise it will have a non-fastforward
    error, since wercker checks out to the hash,
    which on a doubled up deploy is in the past for master

--- run.py
+++ run.py
@@ -32,7 +32,7 @@ def gitflow_release_start_command_string(version):
 def gitflow_release_finish_command_string(version):
   message_text = core.field_flags["WERCKER_FLOWY_RELEASE_TAG_MESSAGE"].replace(" ", "-")
   tag_message = "{0}-{1}".format(message_text, version)
-  return "git flow release finish -k -p -m \"{1}\" {0}".format(version, tag_message)
+  return "git flow release finish -F -k -p -m \"{1}\" {0}".format(version, tag_message)
 
 def complete_release(functor):
   tag = core.get_next_tag(functor)
