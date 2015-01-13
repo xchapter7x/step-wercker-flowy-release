4d3609f7b29a11d67970580155282c78771c661b
John Calabrese <jcalabrese@pivotal.io>
2015-01-13 15:58:37 -0500

    bugfix commit checkout
    making sure we end by checking out the original commit
    also, setting exit code on exit from python script
    run result

--- run.sh
+++ run.sh
@@ -3,13 +3,22 @@
 sudo wget -P /tmp http://launchpadlibrarian.net/84618376/git-flow_0.4.1-2_all.deb
 sudo dpkg -i /tmp/git-flow_0.4.1-2_all.deb
 
+FAILED_RUN=0
+
 python $WERCKER_STEP_ROOT/run.py
+
+if [ $? != 0 ]; then
+  FAILED_RUN=1
+fi
 
 if [ -f .statefile ];then
    source .statefile
 fi
 
-if [ $? != 0 ]; then
+git checkout -fq ${WERCKER_GIT_COMMIT}
+git submodule update --init --recursive
+
+if [ $FAILED_RUN != 0 ]; then
   fail "Failure"
 
 else
